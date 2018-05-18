a3cf8043e8acb96dab52887d92eeb446e248976e
雷杰 <592454407@qq.com>
2018-05-18 18:53:45 +0800

    提交

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,21 @@
  * Created by leijie on 2018/5/12.
  */
 (function () {
+
+	wx.config({
+		debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		appId: _appId, // 必填，公众号的唯一标识
+		timestamp: _timestamp, // 必填，生成签名的时间戳
+		nonceStr: _nonceStr, // 必填，生成签名的随机串
+		signature: _signature,// 必填，签名，见附录1
+		jsApiList: ["chooseImage", "uploadImage"]
+// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	});
+	wx.error(function (res) {
+		alert(res.errMsg);
+	});
+
+
 	/*************store************/
 	var state = {
 		list: '',
